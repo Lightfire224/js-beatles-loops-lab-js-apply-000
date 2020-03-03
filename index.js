e50d37c295da8c89575acd012d8156e4ff1e1277
Elias Taveras <eliastave12@gmail.com>
2020-03-03 17:00:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+
+array = []
+
+function theBeatlesPlay(musicians, instruments){
+  let array = []
+  for (let i=0; i<musicians.length; i++){
+    array.push(musicians[i] + " plays " + instruments[i])
+  }
+  return array
+}
