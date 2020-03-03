0337d72be78115a8a182d2c41dd71da7f1bfa74c
Elias Taveras <eliastave12@gmail.com>
2020-03-03 17:47:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,22 +18,3 @@ function johnLennonFacts(facts){
     i++
   }
 }
-
-2. Create a function `johnLennonFacts`. 
-
-    * This function will accept one argument, an array of facts about John
-      Lennon (note that it might not be exactly the following facts):
-
-    ```js
-    const facts = [
-      "He was the last Beatle to learn to drive",
-      "He was never a vegetarian",
-      "He was a choir boy and boy scout",
-      "He hated the sound of his own voice"
-    ];
-    ```
-
-    * Use a while loop to loop over the facts array and add `"!!!"` to the end of
-    every fact.
-
-    * Return an array of strings with exclamation points.
