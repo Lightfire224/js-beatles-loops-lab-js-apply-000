8e44ad7649793e7ccfa78e7c998f5abcb423132f
Elias Taveras <eliastave12@gmail.com>
2020-03-03 17:48:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function theBeatlesPlay(musicians, instruments){
 function johnLennonFacts(facts){
   let i=0
   while(i<facts.length){
-    console.log(fact[i] + "!!!");
+    console.log(facts[i] + "!!!");
     i++
   }
 }
